a050a47a36ed38d01e741290082a7d3a79ba1c42
ybk5053 <yeowenjie@gmail.com>
2018-09-20 17:30:10 +0800

    hihihi2

--- dbhelper.py
+++ dbhelper.py
@@ -13,56 +13,74 @@ class DBHelper:
         stmt = "CREATE TABLE IF NOT EXISTS mydb (phone_number TEXT, family_name TEXT)"
         self.conn.execute(stmt)
         self.conn.commit()
+        
+    def find_family(self, phone_no):
+        stmt = "SELECT family_name FROM mydb WHERE phone_number LIKE ?"
+        args = (phone_no, )
+        cur = self.conn.cursor()
+        cur.execute(stmt, args)
+        res = "".join("%s" % tup for tup in cur.fetchall())
+        cur.close()
+        return res
 
     #supposed to return eating status of every1 in family
-    def check_status(self, family_name):
+    def check_status(self, phone_no):
+        family_name = self.find_family(phone_no)
+        print(family_name)
         cur = self.conn.cursor()
-        stmt = "SELECT * FROM ?"
-        args = (family_name, )
-        cur.execute(stmt, args)
-        res = cur.fetchall()
+        stmt = "SELECT * FROM " + family_name
+        res = ""
+        for row in cur.execute(stmt):
+            res = res + str(row)
+            res = res + '\n'
+            print(res)
         cur.close()
         return res
 
     #create 1 table for each family
     def add_family(self, family_name):
         cur = self.conn.cursor()
-        stmt = "CREATE TABLE ? (phone number TEXT, name TEXT, eating INTEGER)"
+        stmt = "CREATE TABLE IF NOT EXISTS " + family_name + " (phone_number TEXT, name TEXT, eating INTEGER)"
         args = (family_name, )
-        cur.execute(stmt, args)
+        cur.execute(stmt)
         self.conn.commit()
+        cur.close()
 
         #add member to family table
     def add_family_member(self, phone_no, family_name, name):
             cur = self.conn.cursor()
-            stmt = "INSERT INTO ? VALUES (?, ?, 0)"
-            args = (family_name, phone_no, name)
+            stmt = "INSERT INTO " + family_name + " VALUES (?, ?, 0)"
+            args = (phone_no, name)
             cur.execute(stmt, args)
             #add to mydb as well
             stmt = "INSERT INTO mydb VALUES (?, ?)"
             args = (phone_no, family_name)
             cur.execute(stmt, args)
             self.conn.commit()
+            cur.close()
 
     #set whether eating anot
     #eating: 1 = eating, 0 = unconfirmed, 2 = not eating
-    def set_eating(self, phone_no, eating, family_name):
-        stmt = "UPDATE ? SET eating = ? WHERE phone_number = ?"
-        args = (family_name, eating, phone_no, )
+    def set_eating(self, phone_no, eating):
+        family_name = self.find_family(phone_no)
+        stmt = "UPDATE " + family_name + " SET eating = ? WHERE phone_number = ?"
+        args = (eating, phone_no, )
         cur = self.conn.cursor()
         cur.execute(stmt, args)
         self.conn.commit()
-
-    def find_family(self, phone_no):
-        stmt = "SELECT family_name FROM mydb WHERE phone_number LIKE ?"
-        args = (phone_no, )
+        cur.close()
+        
+    def set_name(self, phone_no, name):
+        family_name = self.find_family(phone_no)
+        stmt = "UPDATE " + family_name + " SET name = ? WHERE phone_number = ?"
+        args = (name, phone_no, )
         cur = self.conn.cursor()
         cur.execute(stmt, args)
-        res = cur.fetchall()
+        self.conn.commit()
         cur.close()
-        return res
         
     def close(self):
         self.conn.close()
+        print("rdyrdy")
 
 
--- telegrambot.py
+++ telegrambot.py
@@ -14,9 +14,18 @@ token = '600148561:AAFHMlyD-by4AdrxTanUFanFjdqchz6syj4'
 #slight change to commit
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 
-DINNERSTATUS = range(1)
 db = DBHelper()
-db.setup();
+db.setup()
+db.close()
+
+waitcreatefamily = []
+waitfamilyname = []
+waitmyname = []
+waitaction = []
+waitdinner = []
+waitadd = []
+waitaddname = []
+waitrmv = []
 
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Hi, I am family dinner bot")
@@ -24,10 +33,10 @@ def start(bot, update):
     #check if in family
     db = DBHelper()
     family_name = db.find_family(update.message.chat_id)
-    print(update.message.chat_id)
     #if no family name = not in family
     #ask if want to create family
     if not family_name:
+        waitcreatefamily.append(update.message.chat_id)
         reply_keyboard = [['Yes', 'No']]
         update.message.reply_text(
             'Hi! You are not in a family yet \n'
@@ -36,14 +45,123 @@ def start(bot, update):
     #if in a family
     #ask to set if eating dinner
     else:
-        print(family_name)
-        reply_keyboard = [['Having Dinner', 'No Dinner', 'Unconfirmed']]
+        waitaction.append(update.message.chat_id)
+        reply_keyboard = [['Dinner?', 'Check family status', 'Add member', 'Remove member', 'Chg Name']]
         update.message.reply_text(
-            'Hi! I am Family Dinner bot! \n'
-            'Tell me, are you having dinner today?',
+            'You are in ' + family_name + " family! \n"
+            'What do you want to do?',
             reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+        
+        
+#        reply_keyboard = [['Having Dinner', 'No Dinner', 'Unconfirmed']]
+#        update.message.reply_text(
+#            'Hi! I am Family Dinner bot! \n'
+#            'Tell me, are you having dinner today?',
+#            reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
     
     db.close()
+    
+def create_family(familyname, myname, id):
+    db = DBHelper()
+    db.add_family(familyname)
+    db.add_family_member(id, familyname, myname)
+    db.close()
+    
+def set_eating(id, eating):
+    db = DBHelper()
+    db.set_eating(id, eating)
+    db.close()
+    
+def familystatus(id):
+    db = DBHelper()
+    res = db.check_status(id)
+    db.close()
+    return res 
+    
+def addmember(myid, newid, name):
+    db = DBHelper()
+    familyname = db.find_family(myid)
+    db.add_family_member(newid, familyname, name)
+    db.close()
+    
+def updatename(id, name):
+    db = DBHelper()
+    db.set_name(id,name)
+    db.close()
+
+def dostuff(bot, update):
+    #when /start when not in family
+    if update.message.chat_id in waitcreatefamily:
+        waitcreatefamily.remove(update.message.chat_id)
+        #if yes send to waitfamilyname queue
+        if update.message.text == "Yes":
+            bot.send_message(chat_id=update.message.chat_id, text="Type a name for your family")
+            waitfamilyname.append(update.message.chat_id)
+            
+    #waiting for name reply
+    elif update.message.chat_id in waitfamilyname:
+        #sets up family in sql
+        waitfamilyname.remove(update.message.chat_id)
+        familyname = update.message.text
+        id = update.message.chat_id
+        create_family(familyname, "", id)
+        bot.send_message(chat_id=update.message.chat_id, text="Congratulations your family has been created\nPlease enter your name")
+        waitmyname.append(update.message,chat_id)
+        
+    elif update.message.chat_id in waitmyname:
+        waitmyname.remove(update.message.chat_id)
+        updatename(update.message.chat_id, update.message.text)
+        bot.send_message(chat_id=update.message.chat_id, text="Done")
+        
+    #when /start when in family 
+    elif update.message.chat_id in waitaction:
+        waitaction.remove(update.message.chat_id)
+        if update.message.text == "Dinner?":
+            #shift to waitdinner queue set key options
+            waitdinner.append(update.message.chat_id)
+            reply_keyboard = [['Having Dinner', 'No Dinner']]
+            update.message.reply_text(
+                'Tell me, are you having dinner today?',
+                reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+        elif update.message.text == "Add member":
+            #shift to waitadd queue
+            waitadd.append(update.message.chat_id)
+            bot.send_message(chat_id=update.message.chat_id, text="Enter the id of your new member")
+        elif update.message.text == "Remove member":
+            #shift to waitrmv queue
+            waitrmv.append(update.message.chat_id)
+            bot.send_message(chat_id=update.message.chat_id, text="Enter the name of the member")
+        elif update.message.text == "Check family status":
+            bot.send_message(chat_id=update.message.chat_id, text=familystatus(update.message.chat_id))
+        elif update.message.text == "Chg Name"
+            waitmyname.append(update.message.chat_id)
+            bot.send_message(chat_id=update.message.chat_id, text="Enter your name")
+            
+            
+    #get response for eating dinner?
+    elif update.message.chat_id in waitdinner:
+        waitdinner.remove(update.message.chat_id)
+        if update.message.text == "Having Dinner":
+            #eating, update sql
+            set_eating(update.message.chat_id, 1)
+            bot.send_message(chat_id=update.message.chat_id, text="Status updated")
+        elif update.message.text == "No Dinner":
+            #not eatin, update sql
+            set_eating(update.message.chat_id, 1)
+            bot.send_message(chat_id=update.message.chat_id, text="Status updated")
+    
+    #waiting for id to add
+    elif update.message.chat_id in waitadd:
+        id = update.message.text
+        addmember(update.message.chat_id, id, "")
+        bot.send_message(chat_id=update.message.chat_id, text="Added")
+        
+    #waiting for name to rmv
+    elif update.message.chat_id in waitrmv:
+        print("nothin")
+    #no action done jus echo
+    else:
+        bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
 
 def help(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text='no help')
@@ -51,11 +169,6 @@ def help(bot, update):
 
 def end(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Family dinner bot never ends")
-
-
-def echo(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
-
 
 def caps(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=text_caps)
@@ -76,26 +189,28 @@ def inline_caps(bot, update):
     bot.answer_inline_query(update.inline_query.id, results)
 
 def unknown(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Sorry, I didn't understand that command.")
+    bot.send_message(chat_id=update.message.chat_id, text=str(update.message.chat_id))
+    
+def getid(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text=str(update.message.chat_id))
 
 
 def main():
-<<<<<<< HEAD
     updater = Updater(token)
 
 
     #get dispatcher to register handlers
     dispatcher = updater.dispatcher
-=======
-	updater = Updater(token)
 
-	#get dispatcher to register handlers
-	dispatcher = updater.dispatcher
->>>>>>> 5399a28e5be8b64685370f7a7be233de6699a702
+    updater = Updater(token)
+
+    #get dispatcher to register handlers
+    dispatcher = updater.dispatcher
 
     #handle commands
+    getid_handler = CommandHandler('id', getid)
     start_handler = CommandHandler('start', start)
-    echo_handler = MessageHandler(Filters.text, echo)
+    reply_handler = MessageHandler(Filters.text, dostuff)
     caps_handler = CommandHandler('caps', caps, pass_args=True)
     inline_caps_handler = InlineQueryHandler(inline_caps)
     unknown_handler = MessageHandler(Filters.command, unknown)
@@ -103,7 +218,7 @@ def main():
     end_handler = CommandHandler('end', end)
 
     dispatcher.add_handler(start_handler)
-    dispatcher.add_handler(echo_handler)
+    dispatcher.add_handler(reply_handler)
     dispatcher.add_handler(caps_handler)
     dispatcher.add_handler(inline_caps_handler)
     dispatcher.add_handler(help_handler)
@@ -113,6 +228,8 @@ def main():
     dispatcher.add_handler(unknown_handler)
 
     updater.start_polling()
+    
+    print("rdy")
 
     updater.idle()
 
