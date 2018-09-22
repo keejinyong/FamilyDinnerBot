44135ae4e7c5adbcd07475a796bb4ed54f69e299
ybk5053 <yeowenjie@gmail.com>
2018-09-22 11:54:31 +0800

    inline buttons

--- dbhelper.py
+++ dbhelper.py
@@ -14,6 +14,15 @@ class DBHelper:
         self.conn.execute(stmt)
         self.conn.commit()
         
+    def create_user(self, phone_no):
+        stmt = "INSERT INTO mydb VALUES (?, null)"
+        args = (phone_no, )
+        cur = self.conn.cursor()
+        cur.execute(stmt, args)
+        self.conn.commit()
+        cur.close()
+        
+        
     def find_family(self, phone_no):
         stmt = "SELECT family_name FROM mydb WHERE phone_number LIKE ?"
         args = (phone_no, )
@@ -21,18 +30,21 @@ class DBHelper:
         cur.execute(stmt, args)
         res = "".join("%s" % tup for tup in cur.fetchall())
         cur.close()
+        if str(res) == "None":
+            return False
+        if not res:
+            self.create_user(phone_no)
         return res
 
     #supposed to return eating status of every1 in family
     def check_status(self, phone_no):
         family_name = self.find_family(phone_no)
         cur = self.conn.cursor()
-        stmt = "SELECT * FROM " + family_name
+        stmt = "SELECT name, eating FROM " + family_name
         res = ""
         for row in cur.execute(stmt):
             res = res + str(row)
             res = res + '\n'
-            print(res)
         cur.close()
         return res
         
@@ -50,24 +62,37 @@ class DBHelper:
     #create 1 table for each family
     def add_family(self, family_name):
         cur = self.conn.cursor()
-        stmt = "CREATE TABLE IF NOT EXISTS " + family_name + " (phone_number TEXT, name TEXT, eating INTEGER)"
+        stmt = "SELECT COUNT (*) FROM mydb WHERE family_name = ?"
+        #SELECT COUNT (*) FROM mydb WHERE phone_number = "411168546"
         args = (family_name, )
-        cur.execute(stmt)
-        self.conn.commit()
+        cur.execute(stmt, args)
+        res = "".join("%s" % tup for tup in cur.fetchall())
+        if int(res) == 0:
+            stmt = "CREATE TABLE IF NOT EXISTS " + family_name + " (phone_number TEXT, name TEXT, eating TEXT)"
+            cur.execute(stmt)
+            self.conn.commit()
+            cur.close()
+            return False
         cur.close()
+        return True
 
         #add member to family table
     def add_family_member(self, phone_no, family_name, name):
+        #check if in family already
+        if not self.find_family(phone_no):
             cur = self.conn.cursor()
             stmt = "INSERT INTO " + family_name + " VALUES (?, ?, 0)"
             args = (phone_no, name)
             cur.execute(stmt, args)
             #add to mydb as well
-            stmt = "INSERT INTO mydb VALUES (?, ?)"
-            args = (phone_no, family_name)
+            stmt = "UPDATE mydb SET family_name = ? WHERE phone_number = ?"
+            args = (family_name, phone_no)
             cur.execute(stmt, args)
             self.conn.commit()
             cur.close()
+            return True
+        else:
+            return False
 
     #set whether eating anot
     #eating: 1 = eating, 0 = unconfirmed, 2 = not eating
@@ -94,17 +119,17 @@ class DBHelper:
         cur = self.conn.cursor()
         for row in cur.execute(stmt):
             res = "".join("%s" % tup for tup in row)
-            stmt2 = "UPDATE " + res + " SET eating = 0"
+            stmt2 = "UPDATE " + res + " SET eating = '0'"
             cur.execute(stmt2)
         self.conn.commit()
         cur.close()
         
     def listofnum(self):
-        stmt = "SELECT phone_number FROM mydb"
+        stmt = "SELECT phone_number FROM mydb WHERE family_name != null"
         cur = self.conn.cursor()
         num = []
         for row in cur.execute(stmt):
-            res = res = "".join("%s" % tup for tup in row)
+            res = "".join("%s" % tup for tup in row)
             num.append(res)
         return num
         
@@ -115,6 +140,36 @@ class DBHelper:
         cur = self.conn.cursor()
         res = "".join("%s" % tup for tup in cur.execute(stmt, args).fetchall())
         return res
+        
+    def removefromfamily(self, phone_no, name):
+        family_name = self.find_family(phone_no)
+        stmt = "SELECT phone_number FROM " + family_name + " WHERE name Like ?"
+        args = (name, )
+        cur = self.conn.cursor()
+        res = ""
+        x = 0
+        for row in cur.execute(stmt, args):
+            line = "".join("%s" % tup for tup in row)
+            res = res + line
+            x = x + 1
+        if not x==1:
+            return False
+        else:
+            stmt4 = "SELECT COUNT (*) FROM " + family_name
+            cur.execute(stmt4)
+            res2 = "".join("%s" % tup for tup in cur.fetchall())
+            if not (int(res2) == 1):
+                stmt2 = "DELETE FROM " + family_name + " WHERE phone_number = " + res
+                cur.execute(stmt2)
+            else:
+                stmt2 = "DROP TABLE " + family_name
+                cur.execute(stmt2)
+            stmt3 = "UPDATE mydb SET family_name = null WHERE phone_number = " + res
+            cur.execute(stmt3)
+            
+            self.conn.commit()
+            cur.close()
+            return True
         
     def close(self):
         self.conn.close()
--- telegrambot.py
+++ telegrambot.py
@@ -1,13 +1,11 @@
 from telegram.ext import Updater
-from telegram.ext import CommandHandler
+from telegram.ext import CommandHandler, CallbackQueryHandler
 from telegram.ext import MessageHandler, Filters
-from telegram import InlineQueryResultArticle, InputTextMessageContent
-from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
-from telegram.ext import InlineQueryHandler
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import JobQueue
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 import logging
-import json
 import datetime
 
 from dbhelper import DBHelper
@@ -43,21 +41,26 @@ def start(bot, update):
     #ask if want to create family
     if not family_name:
         waitcreatefamily.append(update.message.chat_id)
-        reply_keyboard = [['Yes', 'No']]
+        reply_keyboard = [[InlineKeyboardButton('Yes', callback_data='Yes')], 
+            [InlineKeyboardButton('No I want to join a family', callback_data='No I want to join a family')]]
         update.message.reply_text(
             'Hi! You are not in a family yet \n'
             'Do you want to create a new family?',
-            reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+            reply_markup=InlineKeyboardMarkup(reply_keyboard))
     #if in a family
     #ask to set if eating dinner
     else:
         my_name = db.find_myname(update.message.chat_id)
         waitaction.append(update.message.chat_id)
-        reply_keyboard = [['Dinner?', 'Check family status', 'Add member', 'Remove member', 'Chg Name']]
+        reply_keyboard = [[InlineKeyboardButton('Dinner?', callback_data='Dinner?')], 
+            [InlineKeyboardButton('Check family status', callback_data='Check family status')], 
+            [InlineKeyboardButton('Add member', callback_data='Add member')], 
+            [InlineKeyboardButton('Remove member', callback_data='Remove member')], 
+            [InlineKeyboardButton('Chg Name', callback_data='Chg Name')]]
         update.message.reply_text(
             'Hi ' + my_name + '! You are in ' + family_name + " family! \n"
             'What do you want to do?',
-            reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+            reply_markup=InlineKeyboardMarkup(reply_keyboard))
         
         
 #        reply_keyboard = [['Having Dinner', 'No Dinner', 'Unconfirmed']]
@@ -70,9 +73,11 @@ def start(bot, update):
     
 def create_family(familyname, myname, id):
     db = DBHelper()
-    db.add_family(familyname)
-    db.add_family_member(id, familyname, myname)
+    res = db.add_family(familyname)
+    if not res:
+        db.add_family_member(id, familyname, myname)
     db.close()
+    return res
     
 def set_eating(id, eating):
     db = DBHelper()
@@ -88,89 +93,113 @@ def familystatus(id):
 def addmember(myid, newid, name):
     db = DBHelper()
     familyname = db.find_family(myid)
-    db.add_family_member(newid, familyname, name)
+    res = db.add_family_member(newid, familyname, name)
     db.close()
+    return res
     
 def updatename(id, name):
     db = DBHelper()
     db.set_name(id,name)
     db.close()
+    
+def removefromfamily(rmvname, id):
+    db = DBHelper()
+    res = db.removefromfamily(id, rmvname)
+    db.close()
+    return res
 
 def dostuff(bot, update):
-    #when /start when not in family
-    if update.message.chat_id in waitcreatefamily:
-        waitcreatefamily.remove(update.message.chat_id)
-        #if yes send to waitfamilyname queue
-        if update.message.text == "Yes":
-            bot.send_message(chat_id=update.message.chat_id, text="Type a name for your family")
-            waitfamilyname.append(update.message.chat_id)
             
     #waiting for name reply
-    elif update.message.chat_id in waitfamilyname:
+    if update.message.chat_id in waitfamilyname:
         #sets up family in sql
         waitfamilyname.remove(update.message.chat_id)
         familyname = update.message.text
         id = update.message.chat_id
-        create_family(familyname, "", id)
-        bot.send_message(chat_id=update.message.chat_id, text="Congratulations your family has been created\nPlease enter your name")
-        waitmyname.append(update.message.chat_id)
+        if not create_family(familyname, "", id):
+            bot.send_message(chat_id=update.message.chat_id, text="Congratulations your family has been created\nPlease enter your name")
+            waitmyname.append(update.message.chat_id)
+        else:
+            bot.send_message(chat_id=update.message.chat_id, text="Name taken try another 1")
+            waitfamilyname.append(update.message.chat_id)
         
     elif update.message.chat_id in waitmyname:
         waitmyname.remove(update.message.chat_id)
         updatename(update.message.chat_id, update.message.text)
         bot.send_message(chat_id=update.message.chat_id, text="Done")
         
-    #when /start when in family 
-    elif update.message.chat_id in waitaction:
-        waitaction.remove(update.message.chat_id)
-        if update.message.text == "Dinner?":
-            #shift to waitdinner queue set key options
-            waitdinner.append(update.message.chat_id)
-            reply_keyboard = [['Having Dinner', 'No Dinner']]
-            update.message.reply_text(
-                'Tell me, are you having dinner today?',
-                reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
-        elif update.message.text == "Add member":
-            #shift to waitadd queue
-            waitadd.append(update.message.chat_id)
-            bot.send_message(chat_id=update.message.chat_id, text="Enter the id of your new member")
-        elif update.message.text == "Remove member":
-            #shift to waitrmv queue
-            waitrmv.append(update.message.chat_id)
-            bot.send_message(chat_id=update.message.chat_id, text="Enter the name of the member")
-        elif update.message.text == "Check family status":
-            bot.send_message(chat_id=update.message.chat_id, text=familystatus(update.message.chat_id))
-        elif update.message.text == "Chg Name":
-            waitmyname.append(update.message.chat_id)
-            bot.send_message(chat_id=update.message.chat_id, text="Enter your name")
-            
-            
-    #get response for eating dinner?
-    elif update.message.chat_id in waitdinner:
-        waitdinner.remove(update.message.chat_id)
-        if update.message.text == "Having Dinner":
-            #eating, update sql
-            set_eating(update.message.chat_id, 1)
-            bot.send_message(chat_id=update.message.chat_id, text="Status updated")
-        elif update.message.text == "No Dinner":
-            #not eatin, update sql
-            set_eating(update.message.chat_id, 1)
-            bot.send_message(chat_id=update.message.chat_id, text="Status updated")
-    
     #waiting for id to add
     elif update.message.chat_id in waitadd:
         waitadd.remove(update.message.chat_id)
         id = update.message.text
-        addmember(update.message.chat_id, id, "")
-        bot.send_message(chat_id=update.message.chat_id, text="Added")
+        if addmember(update.message.chat_id, id, ""):
+            bot.send_message(chat_id=update.message.chat_id, text="Added")
+        else:
+            bot.send_message(chat_id=update.message.chat_id, text="Already in a family")
         
     #waiting for name to rmv
     elif update.message.chat_id in waitrmv:
         waitrmv.remove(update.message.chat_id)
-        print("nothin")
+        if removefromfamily(update.message.text, update.message.chat_id):
+            bot.send_message(chat_id=update.message.chat_id, text="Removed from family")
+        else:
+            bot.send_message(chat_id=update.message.chat_id, text="Name not found")
     #no action done jus echo
     else:
         bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+        
+def button(bot, update):
+    query = update.callback_query
+    #/start without family
+    if query.message.chat_id in waitcreatefamily:
+        waitcreatefamily.remove(query.message.chat_id)
+        #if yes send to waitfamilyname queue
+        if query.data == "Yes":
+            bot.send_message(chat_id=query.message.chat_id, text="Type a name for your family")
+            waitfamilyname.append(query.message.chat_id)
+        elif query.data == "No I want to join a family":
+            bot.send_message(chat_id=query.message.chat_id, text="Get your family to invite you with your id: " + str(query.message.chat_id))
+            
+    #when /start when in family 
+    elif query.message.chat_id in waitaction:
+        waitaction.remove(query.message.chat_id)
+        if query.data == "Dinner?":
+            #shift to waitdinner queue set key options
+            waitdinner.append(query.message.chat_id)
+            reply_keyboard = [[InlineKeyboardButton('Having Dinner', callback_data='Having Dinner')], 
+                [InlineKeyboardButton('No Dinner', callback_data='No Dinner')]]
+            bot.send_message(chat_id=query.message.chat_id,
+                text='Tell me, are you having dinner today?',
+                reply_markup=InlineKeyboardMarkup(reply_keyboard))
+        elif query.data == "Add member":
+            #shift to waitadd queue
+            waitadd.append(query.message.chat_id)
+            bot.send_message(chat_id=query.message.chat_id, text="Enter the id of your new member")
+        elif query.data == "Remove member":
+            #shift to waitrmv queue
+            waitrmv.append(query.message.chat_id)
+            bot.send_message(chat_id=query.message.chat_id, text="Enter the name of the member")
+        elif query.data == "Check family status":
+            bot.send_message(chat_id=query.message.chat_id, text=familystatus(query.message.chat_id))
+        elif query.data == "Chg Name":
+            waitmyname.append(query.message.chat_id)
+            bot.send_message(chat_id=query.message.chat_id, text="Enter your name")
+            
+    #get response for eating dinner?
+    elif query.message.chat_id in waitdinner:
+        waitdinner.remove(query.message.chat_id)
+        if query.data == "Having Dinner":
+            #eating, update sql
+            set_eating(query.message.chat_id, "Eating")
+            bot.send_message(chat_id=query.message.chat_id, text="Status updated")
+        elif query.data == "No Dinner":
+            #not eatin, update sql
+            set_eating(query.message.chat_id, "Not Eating")
+            bot.send_message(chat_id=query.message.chat_id, text="Status updated")
+            
+    else:
+        bot.send_message(chat_id=query.message.chat_id, text="Use /start to start")
+    
 
 def help(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text='no help')
@@ -179,55 +208,39 @@ def help(bot, update):
 def end(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Family dinner bot never ends")
 
-def caps(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=text_caps)
-
-
-def inline_caps(bot, update):
-    query = update.inline_query.query
-    if not query:
-        return
-    results = list()
-    results.append(
-        InlineQueryResultArticle(
-            id=query.upper(),
-            title='Caps',
-            input_message_content=InputTextMessageContent(query.upper())
-        )
-    )
-    bot.answer_inline_query(update.inline_query.id, results)
-
 def unknown(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=str(update.message.chat_id))
+    bot.send_message(chat_id=update.message.chat_id, text="Your id is: " + str(update.message.chat_id))
     
 def getid(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=str(update.message.chat_id))
+    bot.send_message(chat_id=update.message.chat_id, text="Your id is: " + str(update.message.chat_id))
     
 def resetdinner(bot, job):
-    print("reset at: " + datetime.datetime.now())
+    print("reset at: " + str(datetime.datetime.now()))
     db = DBHelper()
     db.resetdinner()
     db.close()
     
 def remind(bot, job):
-    print("reminder send at: " + datetime.datetime.now())
+    print("reminder send at: " + str(datetime.datetime.now()))
     db = DBHelper()
-    nums = db.litsofnum()
+    nums = db.listofnum()
     for num in nums:
         if db.geteat(num) == "0":
             #send reminder
-            waitdinner.append(update.message.chat_id)
-            reply_keyboard = [['Having Dinner', 'No Dinner']]
-            update.message.reply_text(
-                'You have not tell me yet, are you having dinner today?',
-                reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+            waitdinner.append(int(num))
+            reply_keyboard = [[InlineKeyboardButton('Having Dinner', callback_data='Having Dinner')], 
+                [InlineKeyboardButton('No Dinner', callback_data='No Dinner')]]
+            bot.send_message(chat_id=num, text='You have not tell me yet, are you having dinner today?',
+                reply_markup=InlineKeyboardMarkup(reply_keyboard))
     
 def sendstatus(bot,job):
-    print("status send at: " + datetime.datetime.now())
+    print("status send at: " + str(datetime.datetime.now()))
     db = DBHelper()
-    nums = db.litsofnum()
+    nums = db.listofnum()
+    reply_keyboard = [['/start']]
     for num in nums:
-        bot.send_message(chat_id=num, text=familystatus(num))
+        bot.send_message(chat_id=num, text=familystatus(num),
+            reply_markup=ReplyKeyboardMarkup(reply_keyboard))
     
     
 
@@ -243,23 +256,18 @@ def main():
     dispatcher = updater.dispatcher
 
     #handle commands
-    getid_handler = CommandHandler('id', getid)
     start_handler = CommandHandler('start', start)
+    button_handler = CallbackQueryHandler(button)
     reply_handler = MessageHandler(Filters.text, dostuff)
-    caps_handler = CommandHandler('caps', caps, pass_args=True)
-    inline_caps_handler = InlineQueryHandler(inline_caps)
-    unknown_handler = MessageHandler(Filters.command, unknown)
+    unknown_handler = MessageHandler(Filters.command, getid)
     help_handler = CommandHandler('help', help)
     end_handler = CommandHandler('end', end)
 
+    dispatcher.add_handler(button_handler)
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(reply_handler)
-    dispatcher.add_handler(caps_handler)
-    dispatcher.add_handler(inline_caps_handler)
     dispatcher.add_handler(help_handler)
     dispatcher.add_handler(end_handler)
-
-
     dispatcher.add_handler(unknown_handler)
 
     updater.start_polling()
@@ -268,6 +276,8 @@ def main():
     jobqueue.run_daily(sendstatus, statustime)
     jobqueue.run_daily(remind, remindertime)
     #jobqueue.run_once(resetdinner, 0)
+    #jobqueue.run_once(remind, 0)
+    #jobqueue.run_once(sendstatus, 0)
 
     updater.idle()
 
